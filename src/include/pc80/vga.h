facae5f3e97f3cdaaf3e95af3013e5d8e005d75a
lantange <lantange@163.com>
2019-01-18 15:53:00 +0800

    reason-no

--- src/include/pc80/vga.h
+++ src/include/pc80/vga.h
@@ -12,6 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *add my change
  */
 
 #ifndef VGA_H
